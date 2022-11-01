29c4c88d7f10f93e937b8b1f6bf5f6a5cdfb0d6a
omid <o.moazzmi@gmail.com>
2022-11-01 15:51:07 +0330

    Project Start

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { css } from "@emotion/css";
+const style = css`
+  color: red;
+`;
+const App = () => {
+  return <div className={style}>hi</div>;
+};
+
+export default App;
